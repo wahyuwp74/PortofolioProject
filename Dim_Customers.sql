35223372ea13dde53c45b4bd0ed3a447ffc5ca1f
wahyuwp74 <100511859+wahyuwp74@users.noreply.github.com>
2022-04-08 09:23:20 +0700

    Add files via upload

---
+++ Dim_Customers.sql
@@ -0,0 +1,37 @@
+-- Cleansed Dim_Customers --
+SELECT c.customerkey AS [CustomerKey]
+      --,[GeographyKey]
+      --,[CustomerAlternateKey]
+      --,[Title]
+      ,c.firstname AS [FirstName]
+      --,[MiddleName]
+      ,c.lastname AS [LastName]
+	  , c.firstname + ' ' + c.lastname AS [Full Name]
+      --,[NameStyle]
+      --,[BirthDate]
+      --,[MaritalStatus]
+      --,[Suffix]
+      ,CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS [Gender]
+      --,[EmailAddress]
+      --,[YearlyIncome]
+      --,[TotalChildren]
+      --,[NumberChildrenAtHome]
+      --,[EnglishEducation]
+      --,[SpanishEducation]
+      --,[FrenchEducation]
+      --,[EnglishOccupation]
+      --,[SpanishOccupation]
+      --,[FrenchOccupation]
+      --,[HouseOwnerFlag]
+      --,[NumberCarsOwned]
+      --,[AddressLine1]
+      --,[AddressLine2]
+      --,[Phone]
+      ,c.datefirstpurchase AS [DateFirstPurchase]
+      --,[CommuteDistance]
+	  ,g.city AS [Customer City]
+  FROM 
+	dbo.dimcustomer AS c
+	LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey
+  ORDER BY
+	Customerkey ASC
---
+++ Dim_Date.sql
@@ -0,0 +1,23 @@
+-- Cleansed Dim_DateTable --
+SELECT TOP (1000) [DateKey]
+      ,[FullDateAlternateKey] AS Date
+      --,[DayNumberOfWeek]
+      ,[EnglishDayNameOfWeek] AS Day
+      --,[SpanishDayNameOfWeek]
+      --,[FrenchDayNameOfWeek]
+      --,[DayNumberOfMonth]
+      --,[DayNumberOfYear]
+      --,[WeekNumberOfYear]
+      ,[EnglishMonthName] AS Month
+	  ,LEFT([EnglishMonthName], 3) AS MonthShort
+      --,[SpanishMonthName]
+      --,[FrenchMonthName]
+      ,[MonthNumberOfYear] AS MonthNo
+      ,[CalendarQuarter] AS Quarter
+      ,[CalendarYear] AS Year
+      --,[CalendarSemester]
+      --,[FiscalQuarter]
+      --,[FiscalYear]
+      --,[FiscalSemester]
+  FROM [AdventureWorksDW2019].[dbo].[DimDate]
+  Where CalendarYear >= 2020
